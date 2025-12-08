0db6e3e6aba959d1da877ac45bedc455a7e84d6b
minhtuan <tuan.pham@coderpush.com>
2025-12-08 23:28:41 +0700

    feat: implement dashboard to fetch data from prisma and show it on the UI

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,51 @@
+import { buttonVariants } from "@/components/ui/button";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import Link from "next/link";
 import { redirect } from "next/navigation";
+import { prisma } from "../utils/db";
+import { BlogPostCard } from "@/components/general/BlogPostCard";
+
+async function getData(userId: string) {
+    const data = await prisma.blogPost.findMany({
+        where: {
+            authorId: userId,
+        },
+        orderBy: {
+            createdAt: 'desc',
+        },
+    });
+    
+    return data;
+}
 
 export default async function DashboardRoute() {
+    const {getUser} = getKindeServerSession();
+    const user = await getUser();
+
+    if (!user) {
+        redirect("/api/auth/register");
+    }
+
+    const data = await getData(user.id);
+
     return (
-        <h1>Hello from the dashboard</h1>
+        <div>
+            <div className="flex items-center justify-between mb-4">
+                <h2 className="text-xl font-medium">Your Blog Articles</h2>
+
+                <Link className={buttonVariants()} href="/dashboard/create">
+                Create Post
+                </Link>
+            </div>
+        
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {data.map((item) => (
+                    <div key={item.id}>
+                        <BlogPostCard data={item} key={item.id}/>
+                    </div>
+                ))}
+
+            </div>
+        </div>
     )
 }
